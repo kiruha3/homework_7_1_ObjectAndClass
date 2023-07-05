650a1afb5adb5efc686ef320cac110d4720d5c04
kirill <kiruha3@mail.ru>
2023-07-05 11:45:24 +0700

    change Main and create three books with authors

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author pushkin = new Author("Александр", "Пушкин");
+        Author stivenKing = new Author("Стивен", "Кинг");
+        Author joanRoaling = new Author("Джоан", "Роалинг");
+        Book happyPotter = new Book("Гарри Поттер и узник Азкабана",joanRoaling,2019);
+        Book greenMile = new Book("Зеленая миля",stivenKing,2014);
+        Book aboutking = new Book("Сказка о царе Салтане",pushkin,1831);
     }
 }
